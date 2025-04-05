6f1daa210b6ce5e603c09be35df5cdd013c2a56e
thealphakenya <thealphakenya@gmail.com>
2025-04-05 09:22:29 +0300

    Your commit message

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 import sys
 import os
 
-# Add the app root and backend to sys.path
+# Add the /app directory and /app/backend directory to sys.path
 app_root = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, app_root)  # Ensure app root is the first in the sys.path
+if app_root not in sys.path:
+    sys.path.append(app_root)
 
 backend_path = os.path.join(app_root, 'backend')
-sys.path.insert(0, backend_path)  # Ensure backend is included in sys.path
+if backend_path not in sys.path:
+    sys.path.append(backend_path)
 
 from app import app  # Import the Flask app
