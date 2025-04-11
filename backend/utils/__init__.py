739d4c96b575df9e721fa9d2426491d71ead5521
thealphakenya <thealphakenya@gmail.com>
2025-04-12 00:30:21 +0300

    Your commit message

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,10 +1,10 @@
 from .logger import setup_logger
 from .helpers import format_response, Timer
-from backend.config import Settings  # Importing from the correct config location
+from backend.config import Settings  # Corrected to import Settings
 
 __all__ = [
     "setup_logger",
     "format_response",
     "Timer",
-    "Settings",
+    "Settings",  # Expose Settings instead of load_config
 ]
