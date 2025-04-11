da27b6a74f2a5bc77aed79e8180dbfe6d988bcce
thealphakenya <thealphakenya@gmail.com>
2025-04-11 23:37:42 +0300

    Your commit message

--- backend/trading_logic/__init__.py
+++ backend/trading_logic/__init__.py
@@ -1,5 +1,6 @@
+# backend/trading_logic/__init__.py
 from .order_execution import OrderExecution
-from .trading_logic import TradingLogic
+from .logic import TradingLogic
 
 __all__ = [
     "OrderExecution",
