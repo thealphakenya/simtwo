98f01a83b8cfebd2179ee1e625017b8b73e1b94f
thekingkenya <rovicviccy@gmail.com>
2025-04-13 02:36:50 +0300

    Your commit message

--- backend/ai_models/__init__.py
+++ backend/ai_models/__init__.py
@@ -1,8 +1,15 @@
-from .model import (
-    LSTMTradingModel,
-    GRUTradingModel,
-    TransformerTradingModel,
-    BaseTradingModel,
-    TradingAI,
-    ReinforcementLearning
-)
+# backend/ai_models/__init__.py
+
+from .lstm_trading_model import LSTMTradingModel
+from .gru_trading_model import GRUTradingModel
+from .transformer_trading_model import TransformerTradingModel
+from .trading_ai import TradingAI
+from .reinforcement_learning import ReinforcementLearning
+
+__all__ = [
+    "LSTMTradingModel",
+    "GRUTradingModel",
+    "TransformerTradingModel",
+    "TradingAI",
+    "ReinforcementLearning",
+]
