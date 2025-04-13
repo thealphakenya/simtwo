9fdb914fd9033e65cfcdae06b70f520c14503f7b
thekingkenya <rovicviccy@gmail.com>
2025-04-13 08:24:30 +0300

    Your commit message

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from app import app as flask_app
+from app import app as flask_app  # Import the Flask app
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
 
+# Initialize FastAPI
 app = FastAPI()
 
-# Mount Flask app on root
+# Mount Flask app on root (the Flask app will handle routes)
 app.mount("/", WSGIMiddleware(flask_app))
