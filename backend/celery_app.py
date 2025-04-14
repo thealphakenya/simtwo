145284d5aaf12fad0f5355e946dba39c9d9fd253
thekingkenya <rovicviccy@gmail.com>
2025-04-14 20:40:11 +0300

    Your commit message

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -2,8 +2,8 @@ from celery import Celery
 
 celery_app = Celery(
     "trading_tasks",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker="redis://simtwo_redis:6379/0",  # Change localhost to Redis service name
+    backend="redis://simtwo_redis:6379/0"  # Change localhost to Redis service name
 )
 
 celery_app.conf.update(
