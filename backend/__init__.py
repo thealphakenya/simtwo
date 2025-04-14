926f11c529e248c69a697219b9d83d756421234f
thekingkenya <rovicviccy@gmail.com>
2025-04-14 04:14:57 +0300

    Your commit message

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,3 @@
-# backend/__init__.py
-
 from .ai_models import (
     LSTMTradingModel,
     GRUTradingModel,
@@ -11,6 +9,7 @@ from .core import StatusManager
 from .data import DataFetcher, get_market_data
 from .trading_logic import OrderExecution, TradingLogic
 from .utils import setup_logger, format_response, Timer, get_safe_position_size
+from .exchange.exchange_data import fetch_ohlcv_data  # Include exchange_data import
 
 __all__ = [
     "LSTMTradingModel",
@@ -27,4 +26,5 @@ __all__ = [
     "format_response",
     "Timer",
     "get_safe_position_size",
+    "fetch_ohlcv_data",  # Export fetch_ohlcv_data to make it accessible from the backend module
 ]
